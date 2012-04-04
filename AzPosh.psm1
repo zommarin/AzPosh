5ad39b84965bbc753209a34825ad9cb54f6037a4
Anders Zommarin <anders@zommarin.se>
2012-04-04 15:31:27 +0200

    Module file

---
+++ AzPosh.psm1
@@ -0,0 +1,17 @@
+﻿#
+# PowerShell module with various "useful stuff"
+#
+# This is stuff that does not fit in any other more specific module
+#
+
+$ScriptDir = $MyInvocation.
+
+$CommandFiles = ls $ScriptDir -Recurse -Include *.ps1
+
+foreach ($Script in $CommandFiles) {
+	. $Script
+}
+
+$CmdLetNames = $CommandFiles | % { $_.Name -replace ".ps1$","" }
+
+Export-ModuleMember $CmdLetNames
